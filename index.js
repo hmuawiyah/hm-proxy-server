86175bf3b5004897526f67eefb26b11cc9a5642b
huseinm <macbookpro@huseinm.local>
2022-12-03 13:57:53 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var port = (process.env.PORT || 3000);
 
-app.use("/", proxy());
+// app.use("/", proxy());
 
 console.log("Server running on port...", port);
 app.listen(port);
